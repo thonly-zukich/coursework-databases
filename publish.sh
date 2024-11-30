a886d9cb362072657f3ce4c750e158a03a53d744
thonly-zukich <anton.litokh@midnight.works>
2024-12-01 01:34:34 +0200

    Deploy

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/furaizi/coursework-databases master:gh-pages
+git push -f https://thonly-zukich.github.io/coursework-databases/tree/master
 
 cd -
